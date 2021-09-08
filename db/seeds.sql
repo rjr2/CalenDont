112149c71f37c4e390069d5ff39718318aeebaa8
Ted-Alexander <83848173+Ted-Alexander@users.noreply.github.com>
2021-09-08 18:38:19 -0400

    schema

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (first_name, last_name)
+VALUES ("Elliot", "Smith"),
+       ("Amira", "Afzal"),
+       ("Christoper", "Lee"),
+       ("Verónica", "Rodriguez"),
+       ("Igor", "Ivanov");
+       
+INSERT INTO events (title, schedule, details, cancelled, attendee id)
+VALUES ("Not a Date", 2021-09-24, "Dinner and a movie.", false, "[1, 2]"),
+        ("Beach Party", 2021-10-02, "just normal, legal stuff down by the river.", true, "[3, 4, 5]")
