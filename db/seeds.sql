4a2129e157286f253891f50c7726a68d1b5d2546
Ted-Alexander <83848173+Ted-Alexander@users.noreply.github.com>
2021-09-08 19:18:55 -0400

    change

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,6 +5,6 @@ VALUES ("Elliot", "Smith"),
        ("Verónica", "Rodriguez"),
        ("Igor", "Ivanov");
        
-INSERT INTO events (title, schedule, details, cancelled, attendee id)
-VALUES ("Not a Date", 2021-09-24, "Dinner and a movie.", false, "[1, 2]"),
-        ("Beach Party", 2021-10-02, "just normal, legal stuff down by the river.", true, "[3, 4, 5]")
+INSERT INTO events (title, schedule, details, cancelled, user_id, user2_id)
+VALUES ("Not a Date", 2021-09-24, "Dinner and a movie.", false, 1, 2),
+        ("Beach Party", 2021-10-02, "just normal, legal stuff down by the river.", true, 3, 4)
