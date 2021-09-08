445ce5f739c0405eb1f450bcbb3391674478d5a5
Ted-Alexander <83848173+Ted-Alexander@users.noreply.github.com>
2021-09-08 19:55:52 -0400

    another schema change

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
-INSERT INTO users (first_name, last_name)
-VALUES ("Elliot", "Smith"),
-       ("Amira", "Afzal"),
-       ("Christoper", "Lee"),
-       ("Verónica", "Rodriguez"),
-       ("Igor", "Ivanov");
+INSERT INTO users (username, email, user_password)
+VALUES ("Elliot Smith", "es123@boogle.com", "password"),
+       ("Amira Afzal", "bongle@beegle.com", "password"),
+       ("Christoper Lee", "saurumon@twotowers.ring", "password"),
+       ("Verónica Rodriguez", "humbrum@smump.pump", "password"),
+       ("Igor Ivanov", "iviviviviv", "password");
        
 INSERT INTO events (title, schedule, details, cancelled, user_id, user2_id)
 VALUES ("Not a Date", 2021-09-24, "Dinner and a movie.", false, 1, 2),
