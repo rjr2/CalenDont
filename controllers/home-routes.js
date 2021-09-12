7a609b6fa809c50c6bac9729b712b8dfdfac7d3c
endlessashley <81969558+endlessashley@users.noreply.github.com>
2021-09-12 12:08:15 -0400

    tweak css

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,6 +30,11 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 
+router.get('/about', (res) => {
+  res.render('about');
+})
+
+
 router.get('/event/:id', async (req, res) => {
   // If the user is not logged in, redirect the user to the login page
   if (!req.session.loggedIn) {
