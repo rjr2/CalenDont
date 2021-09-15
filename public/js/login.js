e356041c85fc71e858839966066157ad8a8c4ec7
RJR <80534043+rjr2@users.noreply.github.com>
2021-09-15 13:10:04 -0400

    removed all instances and references to email

--- public/js/login.js
+++ public/js/login.js
@@ -10,13 +10,13 @@ const loginFormHandler = async (event) => {
     if (username && password) {
       const response = await fetch('/api/user/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         document.location.replace('/');
-        console.log("success")
+        console.log(response)
       } else {
         alert('Failed to log in.');
       }
@@ -27,13 +27,12 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && email && password) {
+    if (username && password) {
       const response = await fetch('/api/user', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
