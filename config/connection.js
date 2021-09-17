51ff66f961b8abfbd873904d2b01d06c970a59f3
endlessashley <81969558+endlessashley@users.noreply.github.com>
2021-09-17 12:33:22 -0400

    requiring sequelize in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
-
-const { Sequelize } = require("sequelize/types");
-
+const Sequelize = require('sequelize');
+require('dotenv').config();
 // const sequelize = new Sequelize(
 //   process.env.DB_NAME,
 //   process.env.DB_USER,
