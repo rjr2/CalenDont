158b5b5513c58bf275ef949a2315cc363736b84a
endlessashley <81969558+endlessashley@users.noreply.github.com>
2021-09-17 12:10:46 -0400

    edit sequelize let in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+// let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
